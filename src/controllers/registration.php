93306b5ac4ca8aa379f08509b1eb2ec3da86a521
rEugeneMarkov <117519379+rEugeneMarkov@users.noreply.github.com>
2023-01-16 23:56:13 +0200

    заменил error на message

--- src/controllers/registration.php
+++ src/controllers/registration.php
@@ -10,17 +10,17 @@ class Registration extends Base
     public function index(Request $request): Response
     {
         $post = $request->post;
-        $error = [];
+        $message = [];
         $data = [];
 
         if (!empty($post)) {
             $data = \Models\Registration::getDataFromPost($post);
-            $error = \Models\Registration::validate($data);
-            if (!$error) {
+            $message = \Models\Registration::validate($data);
+            if (!$message) {
                 \Models\User::addUser($data['name'], $data['email'], $data['pass']);
-                $error['success'] = "Вы успешно зарегистрировались!";
+                $message['success'] = "Вы успешно зарегистрировались!";
             }
         }
-        return $this->contentToResponse(['data' => $data, 'error' => $error]);
+        return $this->contentToResponse(['data' => $data, 'message' => $message]);
     }
 }
