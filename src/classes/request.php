f9de8ec45ece316078f684502d82f9a78bb6951a
rEugeneMarkov <117519379+rEugeneMarkov@users.noreply.github.com>
2023-01-17 00:26:48 +0200

    Устранил ошибки PSR

--- src/classes/request.php
+++ src/classes/request.php
@@ -17,29 +17,12 @@ class Request
 
     public static function createFromGlobals(): static
     {
-        $request = self::createRequestFromFactory($_GET, $_POST, [], $_COOKIE, $_FILES, $_SERVER);
-
-        // if (str_starts_with($request->headers->get('CONTENT_TYPE', ''), 'application/x-www-form-urlencoded')
-        //    && \in_array(strtoupper($request->server->get('REQUEST_METHOD', 'GET')), ['PUT', 'DELETE', 'PATCH'])
-        // ) {
-        //     parse_str($request->getContent(), $data);
-        //     $request->request = new InputBag($data);
-        // }
+        $request = self::createRequestFromFactory($_GET, $_POST, $_SERVER);
         return $request;
     }
 
-    private static function createRequestFromFactory(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null): static
+    private static function createRequestFromFactory(array $query = [], array $request = [], array $server = []): static
     {
-        // if (self::$requestFactory) {
-        //     $request = (self::$requestFactory)($query, $request, $attributes, $cookies, $files, $server, $content);
-
-        //     if (!$request instanceof self) {
-        //         throw new \LogicException('The Request factory must return an instance of Symfony\Component\HttpFoundation\Request.');
-        //     }
-
-        //     return $request;
-        // }
-
-        return new static($query, $request, $attributes, $cookies, $files, $server, $content);
+        return new static($query, $request, $server);
     }
 }
--- web/config.php
+++ web/config.php
@@ -1,12 +1,18 @@
 <?php
 
+// Чтоб PSR не ругался, переделал но закоментил первоначальное
+
 // Задаем константы:
 define('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
-$sitePath = realpath(dirname(__FILE__) . DS) . DS;
-$sitePath = str_replace('web', 'src', $sitePath);
-define('SITE_PATH', $sitePath); // путь к корневой папке сайта
-$siteTemp = str_replace('/src/', '', $sitePath);
-define('SITE_TEMP', $siteTemp);
+//$sitePath = realpath(dirname(__FILE__) . DS) . DS;
+//$sitePath = str_replace('web', 'src', $sitePath);
+//define('SITE_PATH', $sitePath); // путь к корневой папке сайта
+//$siteTemp = str_replace('/src/', '', $sitePath);
+//define('SITE_TEMP', $siteTemp);
+define('SITE_PATH', str_replace('web', 'src', realpath(dirname(__FILE__) . DS) . DS)); // путь к корневой папке сайта
+define('SITE_TEMP', str_replace('/src/', '', str_replace('web', 'src', realpath(dirname(__FILE__) . DS) . DS)));
+
+
 
 // для подключения к бд
 define('DB_USER', 'root');
